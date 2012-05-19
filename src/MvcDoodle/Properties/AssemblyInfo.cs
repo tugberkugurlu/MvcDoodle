e34610cf29ccef69e2b628d28fb82a4703750c7b
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-05-19 16:23:00 +0300

    initial dev commit

---
+++ src/MvcDoodle/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MvcDoodle")]
+[assembly: AssemblyDescription("MvcDoodle is an ASP.NET MVC library which contains several useful Extensions, HtmlHelpers, Validation Attributes and so on and so forth.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tugberk Ugurlu")]
+[assembly: AssemblyProduct("MvcDoodle")]
+[assembly: AssemblyCopyright("Copyright © Tugberk Ugurlu 2012")]
+[assembly: AssemblyTrademark("Tugberk Ugurlu")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ca3496ba-6ab5-4f48-96b2-b375bdb7a50a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0")]
