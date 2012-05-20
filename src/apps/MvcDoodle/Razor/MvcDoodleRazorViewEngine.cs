14dc59228225be0dccd27ddb711ce5ddcb7234be
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-05-20 14:33:22 +0300

    added TwoLevelViewCache & MvcDoodleRazorViewEngine

---
+++ src/apps/MvcDoodle/Razor/MvcDoodleRazorViewEngine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace MvcDoodle {
+
+    public class MvcDoodleRazorViewEngine : RazorViewEngine {
+
+        public MvcDoodleRazorViewEngine() {
+
+            base.AreaViewLocationFormats = new string[] { 
+                "~/Areas/{2}/Views/{1}/{0}.cshtml",
+                "~/Areas/{2}/Views/Shared/{0}.cshtml"
+            };
+
+            base.AreaMasterLocationFormats = new string[] { 
+                "~/Areas/{2}/Views/{1}/{0}.cshtml",
+                "~/Areas/{2}/Views/Shared/{0}.cshtml"
+            };
+
+            base.AreaPartialViewLocationFormats = new string[] { 
+                "~/Areas/{2}/Views/{1}/{0}.cshtml",
+                "~/Areas/{2}/Views/Shared/{0}.cshtml"
+            };
+
+            base.ViewLocationFormats = new string[] {
+                "~/Views/{1}/{0}.cshtml",
+                "~/Views/Shared/{0}.cshtml"
+            };
+
+            base.PartialViewLocationFormats = new string[] {
+                "~/Views/{1}/{0}.cshtml",
+                "~/Views/Shared/{0}.cshtml"
+            };
+
+            base.MasterLocationFormats = new string[] {
+                "~/Views/{1}/{0}.cshtml",
+                "~/Views/Shared/{0}.cshtml"
+            };
+
+            base.ViewLocationCache = new TwoLevelViewCache(base.ViewLocationCache);
+        }
+    }
+}
---
+++ src/apps/MvcDoodle/TwoLevelViewCache.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDoodle {
+
+    //http://blogs.msdn.com/b/marcinon/archive/2011/08/16/optimizing-mvc-view-lookup-performance.aspx
+    public class TwoLevelViewCache : IViewLocationCache {
+
+        private readonly static object s_key = new object();
+        private readonly IViewLocationCache _cache;
+
+        public TwoLevelViewCache(IViewLocationCache cache) {
+
+            _cache = cache;
+        }
+
+        public string GetViewLocation(HttpContextBase httpContext, string key) {
+
+            var dictionary = getRequestCache(httpContext);
+
+            string location;
+            if (!dictionary.TryGetValue(key, out location)) {
+
+                location = _cache.GetViewLocation(httpContext, key);
+                dictionary[key] = location;
+            }
+
+            return location;
+        }
+
+        public void InsertViewLocation(HttpContextBase httpContext, string key, string virtualPath) {
+
+            _cache.InsertViewLocation(httpContext, key, virtualPath);
+        }
+
+        private static IDictionary<string, string> getRequestCache(HttpContextBase httpContext) {
+
+            var dictionary = httpContext.Items[s_key] as IDictionary<string, string>;
+            if (dictionary == null) {
+
+                dictionary = new Dictionary<string, string>();
+                httpContext.Items[s_key] = dictionary;
+            }
+
+            return dictionary;
+        }
+    }
+}
