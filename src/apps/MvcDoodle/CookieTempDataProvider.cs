606a9a973e74722cc5a0ef9733f8d289e5f46d31
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-05-20 14:45:36 +0300

    added CookieTempDataProvider

---
+++ src/apps/MvcDoodle/CookieTempDataProvider.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDoodle {
+
+    public class CookieTempDataProvider : ITempDataProvider {
+
+        private HttpContextBase httpContext;
+        const string TempDataCookieKey = "__Controller::TempData";
+
+        private bool CookieHasTempData {
+            get {
+                return ((httpContext.Response != null) && (httpContext.Response.Cookies != null)) && (httpContext.Response.Cookies[TempDataCookieKey] != null);
+            }
+        }
+
+        public HttpContextBase HttpContext {
+
+            get {
+                return httpContext;
+            }
+        }
+
+        public CookieTempDataProvider(HttpContextBase httpContext) {
+
+            if (httpContext == null) {
+                throw new ArgumentNullException("httpContext");
+            }
+            this.httpContext = httpContext;
+        }
+
+        protected virtual IDictionary<string, object> LoadTempData(ControllerContext controllerContext) {
+
+            var cookie = httpContext.Request.Cookies[TempDataCookieKey];
+            var dictionary = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+
+            if ((cookie == null) || String.IsNullOrEmpty(cookie.Value))
+                return dictionary;
+
+            foreach (var key in cookie.Values.AllKeys)
+                dictionary[key] = cookie[key];
+            
+            cookie.Expires = DateTime.MinValue;
+            cookie.Value = String.Empty;
+            if (CookieHasTempData) {
+                cookie.Expires = DateTime.MinValue;
+                cookie.Value = String.Empty;
+            }
+
+            return dictionary;
+        }
+
+        protected virtual void SaveTempData(ControllerContext controllerContext, IDictionary<string, object> values) {
+
+            if (values.Count > 0) {
+
+                var cookie = new HttpCookie(TempDataCookieKey);
+                cookie.HttpOnly = true;
+
+                foreach (var item in values)
+                    cookie[item.Key] = Convert.ToString(item.Value, CultureInfo.InvariantCulture);
+
+                httpContext.Response.Cookies.Add(cookie);
+            }
+        }
+
+        IDictionary<string, object> ITempDataProvider.LoadTempData(ControllerContext controllerContext) {
+
+            return LoadTempData(controllerContext);
+        }
+
+        void ITempDataProvider.SaveTempData(ControllerContext controllerContext, IDictionary<string, object> values) {
+
+            SaveTempData(controllerContext, values);
+        }
+    }
+}
