915f56cbff88e6a685eb9eb9b3739c9ff386ef34
tugberkugurlu <tugberkugurlu@gmail.com>
2012-09-11 01:05:16 +0300

    added Each extension method which returns HelperResult back. Also reference Mvc nuget packages instead of directly referencing them from the GAC

---
+++ src/apps/MvcDoodle/Html/ExtensionsForHelperResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.WebPages;
+
+namespace MvcDoodle.Html {
+
+    public static class ExtensionsForHelperResult {
+
+        public static HelperResult Each<TItem>(
+            this IEnumerable<TItem> items,
+            Func<IndexedItem<TItem>, HelperResult> template) {
+
+            return new HelperResult(
+                writer => {
+
+                    for (int i = 0; i < items.Count(); i++) {
+
+                        template(new IndexedItem<TItem>(i, items.ElementAt(i))).WriteTo(writer);
+                    }
+
+                });
+        }
+    }
+}
---
+++ src/apps/MvcDoodle/Html/IndexedItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcDoodle.Html {
+
+    public class IndexedItem<TModel> {
+
+        public IndexedItem(int index, TModel item) {
+
+            Index = index;
+            Item = item;
+        }
+
+        public int Index { get; private set; }
+        public TModel Item { get; private set; }
+    }
+}
